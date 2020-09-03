90c1b9d11c9a01ab09b6244e1fe7b9230b91ae2d
Sammy Hajhamid <pokeso123@hotmail.com>
2020-09-03 14:40:03 -0700

    mongodb conn

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import chalk from 'chalk';
+import mongoose from 'mongoose';
 
 import './config/env';
 import { expressLogger, logger } from './config/winston';
@@ -7,11 +8,23 @@ import router from './routes/router';
 
 const app: express.Application = express();
 const port: number = +(process.env.PORT || 3000);
+const { MONGO_URI } = process.env;
 
 app.use(express.json());
 app.use('/api', router);
 app.use(expressLogger);
 
-app.listen(port, () => {
-  logger.info(`Express server has started listening on port ${chalk.red(port)}.`);
+mongoose.connect(MONGO_URI || '', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+}, (err) => {
+  if (err) {
+    logger.error(`Mongoose error while connecting to MongoDB: ${err}`);
+    throw err;
+  }
+  app.listen(port, () => {
+    logger.info(`Express server has started listening on port ${chalk.red(port)}.`);
+  });
 });
