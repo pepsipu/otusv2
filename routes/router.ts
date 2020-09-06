4c7645483d4c13e20fb8446582c17f3dfecfd39e
Sammy Hajhamid <pokeso123@hotmail.com>
2020-09-06 01:17:27 -0700

    routing for apis

--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,10 @@
 import express from 'express';
+import users from './users';
+
+const routes = [
+  ...users.routes,
+];
 
 const router = express.Router();
+routes.forEach((route: (router: express.Router) => void) => route(router));
 export default router;
---
+++ routes/users/register.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+export default {
+  routes: [(router: express.Router) => {
+    router.post('users/register', () => {
+
+    });
+  }],
+};
