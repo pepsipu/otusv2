263e007c8e3a7bec6ae9c7734986807dd93385fa
Sammy Hajhamid <pokeso123@hotmail.com>
2020-09-06 20:48:58 -0700

    routing for apis

--- app/src/crypto.rs
+++ app/src/crypto.rs
@@ -1,5 +1,6 @@
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
+use std::fmt::{Debug, Display, Formatter};
 
 pub struct Crypto {
     sha256: Sha256,
@@ -22,12 +23,22 @@ impl Crypto {
 
     /* given string S and the hash and length of substring H, return index of substring H.
     this requires len(S) - len(H) + 1 hashes, an O(n) solution. */
-    pub fn contains(&mut self, s: &[u8], hash: &[u8], l: usize) -> isize {
-        for i in 0..s.len() - l + 1 {
-            if self.hash(&s[i..i + l]) == hash {
-                return i as isize;
-            }
+    pub fn contains(&mut self, s_hashes: &Vec<[u8; 32]>, hash: &[u8]) -> Option<usize> {
+        s_hashes.iter().position(|&h| hash == h)
+    }
+
+    pub fn compute_hashes(&mut self, s: &[u8], l: usize) -> Vec<[u8; 32]> {
+        let hashes_to_compute = s.len() - l + 1;
+        let mut hashes: Vec<[u8; 32]> = Vec::with_capacity(hashes_to_compute);
+        for i in 0..hashes_to_compute {
+            hashes.push(self.hash(&s[i..i + l]));
         }
-        return -1;
+        hashes
+    }
+}
+
+impl Debug for Crypto {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        Ok(())
     }
 }
