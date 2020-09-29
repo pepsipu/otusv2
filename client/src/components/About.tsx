15d5af39b1f2b517b14a8f4862e736e3d76c73d1
Sammy Hajhamid <pokeso123@hotmail.com>
2020-09-29 01:23:03 -0700

    vanta background

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useEffect, useRef, useState } from 'react';
+import './About.css';
+// import { motion } from 'framer-motion';
+
+// vanta pls add typescript typing
+const vantaNet = require('vanta/dist/vanta.net.min').default;
 
 export default () => {
-  const x = 2;
-  return (<p>{x}</p>);
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+  console.log(vantaNet);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(vantaNet({
+        el: vantaRef.current,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: true,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0xf05d5e,
+        backgroundColor: 0x060e0e,
+      }));
+    }
+  }, [vantaEffect]);
+  return (
+    <div id="background" ref={vantaRef} />
+  );
 };
