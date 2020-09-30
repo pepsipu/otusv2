381247d4d457c42f784e346aae64cbcd30eef8ba
Sammy Hajhamid <pokeso123@hotmail.com>
2020-09-29 18:56:48 -0700

    logo and buttons and actually presentable stuff

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './About.css';
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
+import OtusLogo from '../img/otus_engine.svg';
 
 // vanta pls add typescript typing
 const vantaNet = require('vanta/dist/vanta.net.min').default;
@@ -8,7 +9,6 @@ const vantaNet = require('vanta/dist/vanta.net.min').default;
 export default () => {
   const [vantaEffect, setVantaEffect] = useState(0);
   const vantaRef = useRef(null);
-  console.log(vantaNet);
   useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(vantaNet({
@@ -16,16 +16,55 @@ export default () => {
         mouseControls: true,
         touchControls: true,
         gyroControls: true,
-        minHeight: 200.00,
-        minWidth: 200.00,
+        minHeight: 200,
+        minWidth: 200,
         scale: 1.00,
         scaleMobile: 1.00,
+        spacing: 17,
+        points: 9,
         color: 0xf05d5e,
         backgroundColor: 0x060e0e,
       }));
     }
   }, [vantaEffect]);
   return (
-    <div id="background" ref={vantaRef} />
+    <div id="background" ref={vantaRef}>
+      <motion.div
+        id="centerDiv"
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.6, duration: 1, ease: 'easeInOut' }}
+        style={{ opacity: 0, margin: '20px' }}
+      >
+        <motion.img
+          src={OtusLogo}
+          alt="Otus Logo"
+          animate={{
+            y: '-15vh',
+            scale: 0.35,
+          }}
+          transition={{ duration: 1, delay: 1.5, ease: 'easeInOut' }}
+        />
+        <motion.div
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.7, duration: 1, ease: 'easeInOut' }}
+          style={{
+            opacity: 0,
+            transform: 'translate(0, -30vh)',
+          }}
+        >
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            style={{
+              width: '200px',
+              height: '60px',
+              fontSize: '12pt',
+            }}
+          >
+            start hacking »
+          </motion.button>
+        </motion.div>
+      </motion.div>
+    </div>
   );
 };
--- client/src/components/ui/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-
-export default (props: { children: any }) => {
-  const { children, ...buttonProps } = props;
-  // eslint-disable-next-line react/jsx-props-no-spreading,react/button-has-type
-  return <motion.button {...buttonProps}>{children}</motion.button>;
-};
