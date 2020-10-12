5c77f8ce52b3accd48f1916676fd1cfbe0449d86
Sammy Hajhamid <pokeso123@hotmail.com>
2020-10-12 16:20:31 -0700

    added captcha and submit stuff

--- client/src/components/user/Register.tsx
+++ client/src/components/user/Register.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap-4-grid';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import Reaptcha from 'reaptcha';
+import { recaptcha } from '../../config.json';
 
-export default () => (
-  <>
-    <div className="centerField">
-      <h1>register</h1>
-      <small>please dont spam new accounts!</small>
-      <br />
-      <input placeholder="email" />
-      <input placeholder="username" />
-      <input placeholder="password" type="password" />
-      <br />
-      <div className="g-recaptcha" data-sitekey="6LeOYtYZAAAAAJV_ayxNSH6WgcH_XJS3bOHe7aNc" data-theme="dark" />
-      <br />
-      <Link to="register">
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          style={{
-            width: '200px',
-            height: '45px',
-            fontSize: '12pt',
-          }}
-        >
-          submit »
-        </motion.button>
-      </Link>
-    </div>
-  </>
-);
+export default () => {
+  const [verified, setVerified] = useState(false);
+  return (
+    <>
+      <div className="centerField">
+        <h1>register</h1>
+        <small>please dont spam new accounts!</small>
+        <br />
+        <input placeholder="email" />
+        <input placeholder="username" />
+        <input placeholder="password" type="password" />
+        <br />
+        <Reaptcha sitekey={recaptcha} onVerify={() => setVerified(true)} />
+        <br />
+        <Link to="register">
+          <motion.button
+            whileHover={verified ? { scale: 1.1 } : {}}
+            whileTap={verified ? { scale: 0.9 } : {}}
+            style={{
+              width: '200px',
+              height: '45px',
+              fontSize: '12pt',
+              background: verified ? '#f05d5e' : '#2d2d2d',
+            }}
+          >
+            submit »
+          </motion.button>
+        </Link>
+      </div>
+    </>
+  );
+};
