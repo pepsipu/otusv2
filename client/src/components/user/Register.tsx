47596bc75f3f3c5559193f15da8e5f0d0bb62ea9
Sammy Hajhamid <pokeso123@hotmail.com>
2020-10-12 21:41:17 -0700

    disable form if not filled

--- client/src/components/user/Register.tsx
+++ client/src/components/user/Register.tsx
@@ -1,33 +1,42 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import 'bootstrap-4-grid';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Reaptcha from 'reaptcha';
 import { recaptcha } from '../../config.json';
 
+// idk why by ref is defined as { current: null } for some reason
+type Ref = { current: any };
+
 export default () => {
+  const [, forceUpdate] = useState();
   const [verified, setVerified] = useState(false);
+  const emailInput: Ref = useRef(null);
+  const usernameInput: Ref = useRef(null);
+  const passwordInput: Ref = useRef(null);
+  // const fieldsFilled = [emailInput, usernameInput, passwordInput].map((i) => i.current?.value).every((v) => v)
+  const filled = emailInput.current?.value && usernameInput.current?.value && passwordInput.current?.value && verified;
   return (
     <>
       <div className="centerField">
         <h1>register</h1>
         <small>please dont spam new accounts!</small>
         <br />
-        <input placeholder="email" />
-        <input placeholder="username" />
-        <input placeholder="password" type="password" />
+        <input placeholder="email" ref={emailInput} onInput={forceUpdate} />
+        <input placeholder="username" ref={usernameInput} onInput={forceUpdate} />
+        <input placeholder="password" type="password" ref={passwordInput} onInput={forceUpdate} />
         <br />
-        <Reaptcha sitekey={recaptcha} onVerify={() => setVerified(true)} />
+        <Reaptcha sitekey={recaptcha} onVerify={() => setVerified(true)} theme="dark" />
         <br />
         <Link to="register">
           <motion.button
-            whileHover={verified ? { scale: 1.1 } : {}}
-            whileTap={verified ? { scale: 0.9 } : {}}
+            whileHover={filled ? { scale: 1.1 } : {}}
+            whileTap={filled ? { scale: 0.9 } : {}}
             style={{
               width: '200px',
               height: '45px',
               fontSize: '12pt',
-              background: verified ? '#f05d5e' : '#2d2d2d',
+              background: filled ? '#f05d5e' : '#2d2d2d',
             }}
           >
             submit »
