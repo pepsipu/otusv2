11ef7e72957944a14241096f79046063a2bec704
Sammy Hajhamid <pokeso123@hotmail.com>
2020-11-03 21:32:17 -0800

    yep

---
+++ deploy/schemas/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  passwordHash: String,
+  publicId: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+const getUsers = async (filter) => User.find(filter);
+
+module.exports = { User, getUsers };
